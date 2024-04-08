a5f09c85d1a1419a1da18509a88d92c8a5b28b87
anton-gandonio <for.site.joins@gmail.com>
2024-04-09 01:27:07 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container, Typography, Box } from '@mui/material'
+
+export const App = () => {
+  return (
+    <Container sx={{bgcolor: 'tomato', height: '100vh'}}>
+      <Typography>1</Typography>
+      <Box>
+        
+      </Box>
+    </Container>
+  )
+}
